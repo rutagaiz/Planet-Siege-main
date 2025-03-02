6be25f64914a34ca69b21c507b6c71a498233000
rutagaiz <rutagaiz@gmail.com>
2025-03-02 19:48:49 +0200

    Initial commit

---
+++ Assets/Player_movement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+
+    private float movement;
+    public float moveSpeed = 5f;
+    private bool facingRight = true;
+    public Rigidbody2D rb;
+    public float jumpHeight = 5f;   
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+
+        if (movement < 0f && facingRight)
+        {
+            transform.eulerAngles = new Vector3(0f, -180f, 0f);
+            facingRight = false;
+        }
+        else if (movement > 0f && facingRight == false)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            facingRight = true;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+    
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(movement, 0f, 0f) * Time.fixedDeltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Planet Siege
+Mokslinės fantastikos tematikos 2D strateginis žaidimas, kuriame žaidėjas turi apsiginti nuo priešo atakų, 
+naudodamas karių būrius, gynybines struktūras ir taktiką. Kiekvienas lygis vyksta unikalioje planetoje su specifinėmis aplinkos sąlygomis, kurios daro įtaką kovoms.
+Lygiai gali būti laimimi dviem būdais – sunaikinant priešo bazę arba apsigynus ir įveikus bosą. Žaidime yra trijų valiutų sistema: 
+pirmoji naudojama karių samdymui ir spąstams statyti, antroji – karių sustiprinimui, trečioji – veikėjo tobulinimui. 
+
+# Tikslas
+Išgyventi priešų atakas, rinkti patirtį, tobulinti veikėją bei kariuomenę ir galiausiai nugalėti planetos bosą arba sunaikinti priešų bazę.
