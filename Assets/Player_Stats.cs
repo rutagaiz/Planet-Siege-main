e094211816430823254adaebafa381e46e0637ad
Beniuha156 <benasdru@gmail.com>
2025-03-16 06:37:40 -0500

    KOM-26 Character health

--- Assets/Player_Stats.cs
+++ Assets/Player_Stats.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    private Vector2 respawnPoint; // Stores the respawn position
+
 
     [SerializeField]
     int currentHealth, maxHealth, currentExperience, maxExperience, currentLevel, attackDamage, Speed, Currency, skillPoints;
 
-
+    private void Start()
+    {
+        respawnPoint = transform.position; // Set initial spawn point
+    }
 
     private void OnEnable()
     {
@@ -34,5 +39,31 @@ public class PlayerStats : MonoBehaviour
         currentExperience = 0;
         maxExperience += 100;
         currentLevel += 1;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player has died!");
+        // DEATH LOGIC <...>
+
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        transform.position = respawnPoint; // Move player to respawn point
+        currentHealth = maxHealth; // Restore health
+        Debug.Log("Player respawned!");
+
     }
 }
